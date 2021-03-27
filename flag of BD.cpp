2902c30cd7d050ffa2ad791773913454d2d923a1
halim4989 <71658024+halim4989@users.noreply.github.com>
2021-03-27 21:42:46 +0600

    Update flag of BD.cpp

--- flag of BD.cpp
+++ flag of BD.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<graphics.h>
-using namespace std;
+
 int main()
 {
     initwindow(700, 450);
@@ -10,11 +10,10 @@ int main()
     bar(50, 45, 650, 405);
 //    floodfill(51, 46, COLOR(0, 106, 78));
 
-
     setcolor(COLOR(244, 42, 65));
     setfillstyle(SOLID_FILL, COLOR(244, 42, 65));
     circle(320, 225, 120);
     floodfill(320, 225, COLOR(244, 42, 65));
 
-    cin.get();
+    std::cin.get();
 }
